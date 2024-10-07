fb7a8f64ebf1aafa8e18b72ec81f2c1f1236df24
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-07 17:05:03 +1100

    Adding NextJs project

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-row gap-8 row-start-2 items-center sm:items-start">
+        <h1>Aman Kumar </h1>
+        <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <h6>Developer</h6>
+
+      </main>
+<div className="!flex !flex-col">A blog is coming soon!</div>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+      </footer>
+    </div>
+  );
+}
